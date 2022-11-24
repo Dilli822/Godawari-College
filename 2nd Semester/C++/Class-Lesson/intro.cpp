fea78a545550ab35a0f840a39d68371b1ebcf6be
dilli <dillihangrae@gmail.com>
2022-11-24 20:22:53 +0545

    [added]adds using namespace std vs not using namespace std...

--- 2nd Semester/C++/Class-Lesson/intro.cpp
+++ 2nd Semester/C++/Class-Lesson/intro.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 // program with using namespace std 
+//When we use using namespace std into the C++ program, then it does not require writing
+//std:: in front of standard commands throughout the code. Namespace std contains all the
+//classes, objects and functions of the standard C++ library.
 using namespace std;
 int main() {
   string first_name;
