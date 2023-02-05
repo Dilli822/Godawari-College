7d1c9341345e8d3351899061f8a62a180c279b3c
dilli <dillihangrae@gmail.com>
2023-02-05 22:21:19 +0545

    [added] adds c++ practice ...

--- 2nd Semester/OOP:C++/practice/program.cpp
+++ 2nd Semester/OOP:C++/practice/program.cpp
@@ -1,21 +1,73 @@
 #include<iostream>
 using namespace std;
 
-int num;
-// return by reference 
-int &add();
+// global 
+int number;
+
+// call by value it gets only copy of variable
+int swap(int a, int b){
+  int c = a;
+  a = b;
+  b = c;
+}
+
+// pass by reference or call by reference. it will directly change the reference variable value
+int swaps(int &a, int &b){
+  int c = a;
+  a = b;
+  b = c;
+}
+
+// return by reference is suitable for multiple function arguments to modify the same reference or variable
+int &pass();
+
+class Laptop{
+  // default public constructor
+  public:
+  Laptop(){
+    cout << " Default public constructor with zero arguments " << endl;
+  }
+
+  // copy constructor
+  Laptop(const Laptop &copyConstructObj){
+    cout << " This is copy constructor " << endl;
+  }
+
+};
+
+// parameterized constructor
+class Pass{
+  private:
+  int x;
+  int y;
+  public:
+  Pass(int age, int number){
+    x = age;
+    y = number;
+    cout << age << " " << number << endl;
+  }
+};
 
 int main(){
-  add() = 34;
+  int first_number = 1, second_number = 2;
+  swap(first_number, second_number);
+  cout << "after swapping " << first_number << " " << second_number << endl;
+  
+  swaps(first_number, second_number);
+  cout << "after swapping " << first_number << " " << second_number << endl;
+
+  pass() = 2023;
+  cout << pass() << endl;
+
+  // class obj
+  Laptop macbook;
+  Laptop hp = macbook;
+
+  Pass obj(23,31);
+
   return 0;
 }
 
-int &add(){
-  return num;
+int &pass(){
+  return number;
 }
-
-// return by reference means returing pointer to an object which means returning origrinal value rather than duplicate
-// we can modify he same object or function arguments
-// we use & to return the object 
-// multiple function can access and modify the same object
-// retuning to reference means passing pointer to an object
